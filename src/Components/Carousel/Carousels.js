60226bcf3c71de4aa5e66045775be16d80ad3896
Nayem <nayemk900@gmail.com>
2021-06-27 21:46:17 +0600

    Progress Bar added

--- src/Components/Carousel/Carousels.js
+++ src/Components/Carousel/Carousels.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
 import CarouselsContent from './CarouselsContent';
 import './Carousels.css'
+import ProgressBar from '../ProgressBar/ProgressBar';
 
 const Carousels = () => {
     const [blogs, setBlogs] = useState([])
+    const [loading, setLoading]=useState(false)
     const settings = {
         className: "text-center",
         centerMode: true,
@@ -19,18 +21,21 @@ const Carousels = () => {
         const url = 'https://blogger-dairies.herokuapp.com/allBlogs'
         fetch(url)
         .then(res => res.json())
-        .then(data => setBlogs(data))
-
+        .then(data =>{
+            setBlogs(data)
+            setLoading(true)
+        })
 
     }, [])
     return (
         <div className='mt-3'>
             <h1 className='mb-3 mt-4'>Top Latest Blogs</h1>
             <Slider {...settings}>
-                    {
+                    { loading && blogs.length ?
                         blogs.map(propsData => <CarouselsContent propsData={propsData} />)
+                        :
+                        <ProgressBar/>
                     }
-
             </Slider>
         </div>
     );
